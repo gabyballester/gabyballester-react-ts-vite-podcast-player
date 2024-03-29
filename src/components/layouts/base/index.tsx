1ca1443b1b1979c4aa3da61489b05891cc11b3a0
gabi <gaby.ballester@gmail.com>
2024-03-29 10:34:30 +0100

    Add link to title and some styling on ball, title, and color vars

--- src/components/layouts/base/index.tsx
+++ src/components/layouts/base/index.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { TransitionBall } from "../../transition-ball";
 import "./styles.scss";
 
@@ -6,7 +6,9 @@ export const BaseLayout = () => {
   return (
     <div className="base-layout">
       <header className="base-layout__header">
-        <p className="base-layout__title">Podcaster</p>
+        <Link to="/" className="base-layout__title">
+          Podcaster
+        </Link>
         <TransitionBall isLoading={true} />
       </header>
       <main className="base-layout__main">
